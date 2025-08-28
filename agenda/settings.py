9079fb78e560900228f3eb2bc9a1e3412572ee1b
Pc1 <>
2025-08-28 15:56:17 -0300

    sei la

--- agenda/settings.py
+++ agenda/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'fornecedores'
+
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
