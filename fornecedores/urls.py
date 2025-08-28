dc33d1f8829b4a8f7aa13b0ab1e559438706ba6d
= <=>
2025-08-28 16:26:30 -0300

    nao existem fornecedores

---
+++ fornecedores/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import FornecedoresView
+
+urlpatterns = [
+    path('fornecedores', FornecedoresView.as_view(), name='fornecedores'),
+]
--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Fornecedor
 
-# Create your views here.
+class FornecedoresView(ListView):
+    model = Fornecedor
+    template_name = 'fornecedores.html'
+
+    def get_queryset(self):
+        buscar = self.request.GET.get('buscar')
+        qs = super(FornecedoresView, self).get_queryset()
+        if buscar:
+            return qs.filter(nome__icontains=buscar)
+        return qs
