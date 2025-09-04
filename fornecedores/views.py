2c44426f2477608071335a8063f7d3ff059bc1e5
pc1 <miguelcalistors@gmail.com>
2025-09-04 14:23:11 -0300

    crud fornecedorew

--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,6 +1,11 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+
+
 
 from .forms import FornecedorModelForm
 from .models import Fornecedor
@@ -14,7 +19,15 @@ class FornecedoresView(ListView):
         qs = super(FornecedoresView, self).get_queryset()
         if buscar:
             return qs.filter(nome__icontains=buscar)
-        return qs
+        if qs.count() > 0:
+            paginator = Paginator(qs, per_page=1)
+            listagem = paginator.get_page(self.request.GET.get('page'))
+            return listagem
+        else:
+            return messages.info(self.request, message='Não existem fornecedores cadastrados!')
+
+
+
 
 class FornecedorAddView(CreateView):
     model = Fornecedor
@@ -33,3 +46,23 @@ class FornecedorDeleteView(DeleteView):
     model = Fornecedor
     template_name = 'fornecedor_apagar.html'
     success_url = reverse_lazy('fornecedores')
+
+    class FornecedorAddView(SuccessMessageMixin, CreateView):
+        model = Fornecedor
+        form_class = FornecedorModelForm
+        template_name = 'fornecedor_form.html'
+        success_url = reverse_lazy('fornecedores')
+        success_message = 'Fornecedor cadastrado com sucesso!'
+
+    class FornecedorUpdateView(SuccessMessageMixin, UpdateView):
+        model = Fornecedor
+        form_class = FornecedorModelForm
+        template_name = 'fornecedor_form.html'
+        success_url = reverse_lazy('fornecedores')
+        success_message = 'Fornecedor alterado com sucesso!'
+
+    class FornecedorDeleteView(SuccessMessageMixin, DeleteView):
+        model = Fornecedor
+        template_name = 'fornecedor_apagar.html'
+        success_url = reverse_lazy('fornecedores')
+        success_message = 'Fornecedor apagado com sucesso!'
